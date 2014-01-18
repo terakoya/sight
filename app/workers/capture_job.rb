18954857ad5776606559113ebecebad01a8c5d04
Shota Fukumori (sora_h) <her@sorah.jp>
2014-01-18 16:13:41 +0900

    Resque

--- app/workers/capture_job.rb
+++ app/workers/capture_job.rb
@@ -1,11 +1,12 @@
 require 'digest/md5'
 class CaptureJob
+  @queue = :capture
   RASTERIZE_JS = Rails.root.join('lib/js/rasterize.js').to_s
   OUT_DIR = Rails.root.join('public/captures')
   OUT_PATH = "/captures"
   Dir.mkdir(OUT_DIR.to_s) unless OUT_DIR.directory?
 
-  def capture(url: )
+  def self.perform(url)
     file_name = "#{Digest::MD5.hexdigest(url)}_#{Time.now.to_i}.png"
     file_path = OUT_DIR.join(file_name).to_s
     buf = IO.popen(['phantomjs', RASTERIZE_JS, url, file_path, err: :out], 'r') do |io|

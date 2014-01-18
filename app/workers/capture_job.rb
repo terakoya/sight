6bd1021ae2b10eb0bf82eecb73cfec01d252b7bc
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-01-18 15:46:33 +0900

    unko

--- app/workers/capture_job.rb
+++ app/workers/capture_job.rb
@@ -1,5 +1,8 @@
+require 'digest/md5'
 class CaptureJob
+  RASTERIZE_JS = Rails.root.join('lib/js/rasterize.js').to_s
   def capture(url: )
-    p url
+    file_path = "#{Digest::MD5.hexdigest(url)}_#{Time.now.to_i}.png"
+    system 'phatomjs', RASTERIZE_JS, url, file_path
   end
 end

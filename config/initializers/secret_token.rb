a6725f9fc68697adb295498c16f9ceff8c81938e
Shota Fukumori (sora_h) <her@sorah.jp>
2014-01-18 15:09:57 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sight::Application.config.secret_key_base = '451a9e316fcb8902b75376513c9749d8b3f3797c13195599c59959e8764025ecb73ce84c7ac41819b57bd4e4b8ffa9c0aa851903b91ffa0498e1fd2736f7e465'

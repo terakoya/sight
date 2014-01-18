81f351ee8d812a1592427a45faa4cf2023979c9c
Shota Fukumori (sora_h) <her@sorah.jp>
2014-01-18 15:54:35 +0900

    Fix to work
    - Raise error with stdout/err when phantomjs failed
    - Create capture record

--- app/workers/capture_job.rb
+++ app/workers/capture_job.rb
@@ -1,8 +1,18 @@
 require 'digest/md5'
 class CaptureJob
   RASTERIZE_JS = Rails.root.join('lib/js/rasterize.js').to_s
+  OUT_DIR = Rails.root.join('public/captures')
+  OUT_PATH = "/captures"
+  Dir.mkdir(OUT_DIR.to_s) unless OUT_DIR.directory?
+
   def capture(url: )
-    file_path = "#{Digest::MD5.hexdigest(url)}_#{Time.now.to_i}.png"
-    system 'phatomjs', RASTERIZE_JS, url, file_path
+    file_name = "#{Digest::MD5.hexdigest(url)}_#{Time.now.to_i}.png"
+    file_path = OUT_DIR.join(file_name).to_s
+    buf = IO.popen(['phantomjs', RASTERIZE_JS, url, file_path, err: :out], 'r') do |io|
+      io.read
+    end
+    raise buf.inspect unless $?.success?
+
+    Capture.create!(captured_url: url, image_url: "#{OUT_PATH}/#{file_name}")
   end
 end
